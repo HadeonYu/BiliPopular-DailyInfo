e7e6ea4f5cb59aae8be1eb426a0e8cda38747a21
Hadeon Yu <hdYu65535@foxmail.com>
2023-08-28 23:34:19 +0800

    2023-08-28

--- code/main.py
+++ code/main.py
@@ -2,6 +2,6 @@ import Collect
 import Process
 
 if __name__ == "__main__":
-    #Collect.getData()
-    Process.paths.changeDate('2023-08-25')
+    Collect.getData()
+    #Process.paths.changeDate('2023-08-25')
     Process.process()
