00cd667f344028f2f4283248761166b5ffebda6f
Hadeon Yu <hdYu65535@foxmail.com>
2023-09-11 09:11:49 +0800

    2023-09-10

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import Process
 import Doc
 
 if __name__ == "__main__":
+    Collect.Paths.changeDate('2023-09-10')
     videoNum = Collect.getData()
     Process.process()
     Doc.makeDoc(videoNum, False)
